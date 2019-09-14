6fc9e04ed1c4a2a3e265c8298bad8e4aa8da67d7
DingPengwei <www.dingpengwei@foxmail.com>
2019-09-14 09:23:50 +0800

    init access and logger and monitor

---
+++ access/src/main/java/pwd/initializr/access/AccessApplication.java
@@ -0,0 +1,26 @@
+package pwd.initializr.access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * pwd.initializr.account@ms-web-initializr
+ *
+ * <h1>TODO what you want to do?</h1>
+ *
+ * date 2019-09-13 22:48
+ *
+ * @author DingPengwei[www.dingpengwei@foxmail.com]
+ * @version 1.0.0
+ * @since DistributionVersion
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class AccessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccessApplication.class, args);
+    }
+}
