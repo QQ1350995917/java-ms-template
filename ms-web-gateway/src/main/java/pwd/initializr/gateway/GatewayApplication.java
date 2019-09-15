371d008d7d9dc1b27698bf9c7243617d0705c23f
DingPengwei <www.dingpengwei@foxmail.com>
2019-09-15 18:05:30 +0800

    init gateway stash

---
+++ ms-web-gateway/src/main/java/pwd/initializr/gateway/GatewayApplication.java
@@ -0,0 +1,26 @@
+package pwd.initializr.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * pwd.initializr.gateway@ms-web-initializr
+ *
+ * <h1>TODO what you want to do?</h1>
+ *
+ * date 2019-09-15 17:52
+ *
+ * @author DingPengwei[www.dingpengwei@foxmail.com]
+ * @version 1.0.0
+ * @since DistributionVersion
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
---
+++ ms-web-gateway/src/test/java/pwd/initializr/gateway/test/GatewayApplicationTest.java
@@ -0,0 +1,26 @@
+package pwd.initializr.gateway.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * pwd.initializr.gateway.test@ms-web-initializr
+ *
+ * <h1>TODO what you want to do?</h1>
+ *
+ * date 2019-09-15 17:53
+ *
+ * @author DingPengwei[www.dingpengwei@foxmail.com]
+ * @version 1.0.0
+ * @since DistributionVersion
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GatewayApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
