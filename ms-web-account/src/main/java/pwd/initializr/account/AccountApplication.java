203133d247dd3e0ef964bf5545a388fa52ebbb25
DingPengwei <www.dingpengwei@foxmail.com>
2019-09-19 15:58:24 +0800

    response vcode image to response stream

--- ms-web-account/src/main/java/pwd/initializr/account/AccountApplication.java
+++ ms-web-account/src/main/java/pwd/initializr/account/AccountApplication.java
@@ -1,11 +1,22 @@
 package pwd.initializr.account;
 
+import com.google.code.kaptcha.Constants;
+import io.swagger.annotations.ApiOperation;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pwd.initializr.common.vcode.ArithmeticCode;
+import pwd.initializr.common.vcode.CodeMessage;
+import pwd.initializr.common.vcode.VCodeHelper;
 
 /**
  * pwd.initializr.account@ms-web-initializr
@@ -41,5 +52,35 @@ public class AccountApplication {
     @GetMapping(value = "/account")
     public String index2() {
         return "this is account 2 index";
+    }
+
+    private VCodeHelper vCodeHelper = new ArithmeticCode();
+
+    @ApiOperation(value = "获取一个验证码")
+    @GetMapping(value = {"/vcode"}, produces = "application/json;charset=UTF-8")
+    public void applyVCode(HttpServletRequest request, HttpServletResponse response) {
+        response.setDateHeader("Expires", 0);
+        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+        response.addHeader("Cache-Control", "post-check=0, pre-check=0");
+        response.setHeader("Pragma", "no-cache");
+        response.setContentType("image/jpeg");
+        CodeMessage codeMessage = vCodeHelper.productMessage();
+        request.getSession().setAttribute(Constants.KAPTCHA_SESSION_KEY, codeMessage.getExpected());
+        BufferedImage bufferedImage = vCodeHelper.productImage(codeMessage.getPresented());
+        ServletOutputStream out = null;
+        try {
+            out = response.getOutputStream();
+            ImageIO.write(bufferedImage, "jpg", out);
+            out.flush();
+        } catch (IOException e) {
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
