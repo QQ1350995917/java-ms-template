61eb6d94a08fda25930ea97390ef8f994d2081cc
DingPengwei <dingpengwei@foxmail.com>
2019-12-16 12:17:07 +0800

    stash

--- ms-web-gateway/bin/start.sh
+++ ms-web-gateway/bin/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 rm -f pid
  # nohup java -jar ms-web-gateway-0.0.1-SNAPSHOT.jar >/dev/null 2>&1  & 
-nohup java -Xms30m -Xmx60m -XX:NewSize=8m -XX:MaxNewSize=16m -jar ms-web-gateway-0.0.1-SNAPSHOT.jar > console.out 2>&1  &
+nohup java -Xms16m -Xmx32m -XX:NewSize=4m -XX:MaxNewSize=8m -jar ms-web-gateway-0.0.1-SNAPSHOT.jar > console.out 2>&1  &
   echo $! > pid 
 echo "server is runing" 
--- ms-web-organization/src/main/java/pwd/initializr/organization/OrganizationApplication.java
+++ ms-web-organization/src/main/java/pwd/initializr/organization/OrganizationApplication.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pwd.initializr.common.web.api.FullPathNameGenerator;
 
 /**
  * pwd.initializr.organization@ms-web-initializr
