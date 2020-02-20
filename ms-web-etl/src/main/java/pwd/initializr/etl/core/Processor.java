5a5b64e6cd45ee56c7c73c1cfca8af3d04f87f29
DingPengwei <www.dingpengwei@foxmail.com>
2020-02-20 17:27:37 +0800

    stash

---
+++ ms-web-etl/src/main/java/pwd/initializr/etl/core/Processor.java
@@ -0,0 +1,16 @@
+package pwd.initializr.etl.core;
+
+/**
+ * pwd.initializr.etl.core@ms-web-initializr
+ *
+ * <h1>TODO what you want to do?</h1>
+ *
+ * date 2020-02-16 15:54
+ *
+ * @author DingPengwei[www.dingpengwei@foxmail.com]
+ * @version 1.0.0
+ * @since DistributionVersion
+ */
+public interface Processor {
+
+}
