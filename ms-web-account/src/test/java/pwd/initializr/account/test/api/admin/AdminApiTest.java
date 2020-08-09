67820b29dfbe052e5758b943f750d9e3fea93464
DingPengwei <www.dingpengwei@foxmail.com>
2020-08-09 21:49:55 +0800

    stash

--- ms-web-account/src/test/java/pwd/initializr/account/test/api/admin/AdminApiTest.java
+++ ms-web-account/src/test/java/pwd/initializr/account/test/api/admin/AdminApiTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import pwd.initializr.account.api.admin.vo.AdminAccountInput;
@@ -61,7 +62,7 @@ public class AdminApiTest {
         MockMvcRequestBuilders.post("/api/admin/admin")
             .contentType(MediaType.APPLICATION_JSON).content(JSON.toJSONString(input))
     )
-        //.andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.status().isOk())
         .andDo(MockMvcResultHandlers.print())
         .andReturn();
     String response = mvcResult.getResponse().getContentAsString();
