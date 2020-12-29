b21021b902107325d5712ded374a0420c76cd25f
DingPengwei <dingpengwei@foxmail.com>
2020-12-29 17:03:10 +0800

    limiter app

--- ms-web-gateway/src/main/java/pwd/initializr/gateway/business/limiter/ServiceKeyResolver.java
+++ ms-web-gateway/src/main/java/pwd/initializr/gateway/business/limiter/ServiceKeyResolver.java
@@ -1,7 +1,6 @@
 package pwd.initializr.gateway.business.limiter;
 
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -11,23 +10,18 @@ import reactor.core.publisher.Mono;
  *
  * <h1>TODO what you want to do?</h1>
  *
- * date 2020-12-12 10:53
+ * date 2020-12-29 16:46
  *
  * @author DingPengwei[dingpengwei@foxmail.com]
  * @version 1.0.0
  * @since DistributionVersion
  */
-@Primary
-@Component("uriKeyResolver")
-public class UriKeyResolver implements KeyResolver {
+@Component("appKeyResolver")
+public class ServiceKeyResolver implements KeyResolver {
 
     @Override
     public Mono<String> resolve(ServerWebExchange exchange) {
-        String url = exchange.getRequest().getURI().toString();
-        if (url.contains("?")) {
-            return Mono.just(url.split("\\?")[0]);
-        } else {
-            return Mono.just(url);
-        }
+        return Mono.just(exchange.getRequest().getURI().getPath());
     }
+
 }
