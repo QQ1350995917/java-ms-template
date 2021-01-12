d6bc9834175a7202ca50f97d1f2ef5391d16be93
DingPengwei <dingpengwei@foxmail.com>
2021-01-12 16:42:48 +0800

    jvm option

--- ms-web-gateway/bin/start.sh
+++ ms-web-gateway/bin/start.sh
@@ -1,6 +1,40 @@
-#!/bin/sh 
-rm -f pid
- # nohup java -jar ms-web-gateway-0.0.1-SNAPSHOT.jar >/dev/null 2>&1  & 
-nohup java -Xms16m -Xmx32m -XX:NewSize=4m -XX:MaxNewSize=8m -jar ms-web-gateway-0.0.1-SNAPSHOT.jar > console.out 2>&1  &
-  echo $! > pid 
-echo "server is runing" 
+#!/bin/bash
+
+# jar包名称
+JAR_NAME="`pwd`/ms-web-gateway-0.0.1-SNAPSHOT.jar"
+echo "JAR FILE PATH: $JAR_NAME"
+
+# profile
+PROFILE_NAME=$1
+if [ -z $PROFILE_NAME ]; then
+	PROFILE_NAME="default"
+fi
+PROFILE_OPTS="--spring.profiles.active=$PROFILE_NAME"
+
+# 记录pid的文件名
+PID_FILE="$PROFILE_NAME.pid"
+rm -rf $PID_FILE
+
+# 日志目录
+DIR_LOG="`pwd`/logs"
+if [ ! -d $DIR_LOG ]; then
+	mkdir $DIR_LOG
+fi
+STDOUT_FILE="$DIR_LOG/console-$PROFILE_NAME.out"
+if [ -f $STDOUT_FILE ]; then
+	STDOUT_FILE_BAK="$DIR_LOG/"console-$PROFILE_NAME-`date +%Y%m%d%H%M%S`.out" "
+	mv $STDOUT_FILE $STDOUT_FILE_BAK
+fi
+echo "STDOUT: $STDOUT_FILE"
+
+# 启动内存
+JAVA_MEM_OPTS="-XX:MaxMetaspaceSize=128m -Xms1G -Xms1G -Xmx1G -XX:NewRatio=1 -XX:SurvivorRatio=6 -XX:MaxDirectMemorySize=64m -Xss1024K -XX:ParallelGCThreads=20 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+UseCMSCompactAtFullCollection -XX:CMSInitiatingOccupancyFraction=80 -Xloggc:./logs/gc.log"
+echo "JAVA_MEM_OPTS: $JAVA_MEM_OPTS"
+#JAVA_JMX_OPTS="-Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=0.0.0.0 -Dcom.sun.management.jmxremote.port=11220 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+
+# 启动
+nohup java $JAVA_MEM_OPTS $JAVA_JMX_OPTS -jar $JAR_NAME $PROFILE_OPTS > $STDOUT_FILE 2>&1 &
+
+# 将pid写入文件
+echo $! > $PID_FILE
+
--- ms-web-gateway/bin/stop.sh
+++ ms-web-gateway/bin/stop.sh
@@ -1,6 +1,20 @@
-tpid=`cat pid | awk '{print $1}'` 
-tpid=`ps -aef | grep $tpid | awk '{print $2}' |grep $tpid`
- if [ ${tpid} ]; then 
-        kill -9 $tpid 
+#!/bin/bash
+
+# profile
+PROFILE_NAME=$1
+if [ -z $PROFILE_NAME ]; then
+  PROFILE_NAME="default"
 fi
-echo "server is stop"
+
+# 查找pid
+PID=`cat $PROFILE_NAME.pid | awk '{print $1}'`
+PID_EXISTS=`ps -ef|grep java | grep "$PID" | awk '{print $2}'`
+
+if [ -z "$PID_EXISTS" ]; then
+  echo "ERROR: The server does not started!"
+  exit 1
+fi
+
+echo -e "STATUS: Stopping, PID: $PID"
+kill $PID > /dev/null 2>&1
+echo -e "STATUS: Stopped, PID: $PID"
