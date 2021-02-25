b0a5d6b71a4f713d25eb74514d779048097e3bf6
DingPengwei <dingpengwei@foxmail.com>
2021-02-25 18:41:41 +0800

    stash

---
+++ ms-web-search/src/main/java/pwd/initializr/search/api/admin/IndexApi.java
@@ -0,0 +1,28 @@
+package pwd.initializr.organization.api;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * pwd-initializr-organization@ms-web-initializr
+ *
+ * <h1>界面</h1>
+ *
+ * date 2021-02-22 21:33
+ *
+ * @author Automatic[dingpengwei@foxmail.com]
+ * @version 0.0.1-SNAPSHOT
+ * @since 0.0.1-SNAPSHOT
+ */
+@Api(
+    tags = "Index",
+    value = "IndexPage",
+    description = "[界面]"
+)
+
+@Controller(value = "Index")
+@RequestMapping(value = "")
+public interface IndexApi {
+
+}
---
+++ ms-web-search/src/main/java/pwd/initializr/search/api/admin/vo/IndexVO.java
@@ -0,0 +1,16 @@
+package pwd.initializr.search.api.admin.vo;
+
+/**
+ * pwd.initializr.search.api.admin.vo@ms-web-initializr
+ *
+ * <h1>TODO what you want to do?</h1>
+ *
+ * date 2021-02-25 15:40
+ *
+ * @author DingPengwei[dingpengwei@foxmail.com]
+ * @version 1.0.0
+ * @since DistributionVersion
+ */
+public class IndexVO {
+
+}
