0a309532948a56b641981b0612f54d6e01797bee
DingPengwei <www.dingpengwei@foxmail.com>
2021-09-13 00:00:26 +0800

    build mulit

--- ms-web-registry/bin/start.sh
+++ ms-web-registry/bin/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh 
 rm -f pid
 nohup java -Xms16m -Xmx32m -XX:NewSize=4m -XX:MaxNewSize=8m -jar ms-web-registry-0.0.1-SNAPSHOT.jar > console.out 2>&1  &
+
   echo $! > pid 
 echo "server is runing" 
